73bebc40a4c83e96b93eaab45ac93ee529482e25
Javohir <xasanof17@gmail.com>
2023-02-28 13:11:47 +0500

    chat changes

--- src/app/Aside.tsx
+++ src/app/Aside.tsx
@@ -3,14 +3,13 @@ import React, { useState } from "react";
 import { IoMdAdd } from "react-icons/io";
 import { BsArrowBarLeft } from "react-icons/bs";
 import { RxHamburgerMenu } from "react-icons/rx";
-import { IconButton, Button} from '@mui/material';
 
 const ButtonChat = () => {
   return (
-    <Button className="p-2 text-left text-grey font-normal text-base border-2 border-dark outline-none w-full rounded-xl capitalize flex items-center space-x-3">
+    <button className="p-2 text-left text-grey font-normal hover:bg-slate-800 focus:border-blue focus:bg-slate-800 hover:border-blue text-base border-2 border-dark outline-none w-full rounded-xl capitalize flex items-center space-x-3">
       <IoMdAdd fontSize={18} color="#dbdbe2" />
       <span>New Chat</span>
-    </Button>
+    </button>
   );
 };
 
@@ -18,17 +17,19 @@ const Aside = () => {
   const [show, setShow] = useState(false);
   return (
     <aside className="lg:w-72 w-0 bg-black h-full min-h-screen relative">
-      <IconButton className="lg:hidden flex items-center justify-center fixed top-5 left-3 z-50">
-        <RxHamburgerMenu fontSize={25} color="#dbdbe2" />
-      </IconButton>
+      <div className="lg:hidden flex items-center justify-center fixed top-5 left-3 z-50">
+        <button>
+          <RxHamburgerMenu fontSize={28} color="#dbdbe2" />
+        </button>
+      </div>
       <div className="absolute top-0 left-0 w-full p-3">
         <ButtonChat />
       </div>
       <div className="absolute bottom-0 left-0 w-full p-3">
-        <Button className="p-2 text-left text-grey font-normal text-base outline-none w-full capitalize flex items-center space-x-3">
+        <button className="p-2 text-left text-grey font-normal hover:bg-slate-800 rounded-xl text-base outline-none w-full capitalize flex items-center space-x-3">
           <BsArrowBarLeft fontSize={20} color="#dbdbe2" />
           <span>Log Out</span>
-        </Button>
+        </button>
       </div>
     </aside>
   );
--- src/app/Send.tsx
+++ src/app/Send.tsx
@@ -3,18 +3,18 @@ import { IoSend } from "react-icons/io5";
 
 const Send = () => {
   return (
-    <div className="absolute bottom-5 left-0 w-full">
-      <form className="mx-5 flex space-x-1 items-center">
+    <div className="absolute bottom-4 left-0 w-full">
+      <form className="mx-1 sm:mx-5 flex space-x-1 items-center">
         <textarea
           rows={1}
           autoFocus
           placeholder="Ask anything..."
           name="message"
-          className="rounded-md resize-none shadow-xl text-grey bg-transparent block p-2 w-full text-[16px] bg-gray-50 outline-none border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="rounded-md resize-none shadow-xl text-grey block p-2 w-full text-[16px] bg-dark outline-none border-2 border-transparent focus:ring-blue focus:bg-light focus:border-blue"
         />
         <button
           type="submit"
-          className="flex items-center justify-center p-2 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600"
+          className="flex items-center justify-center p-2 rounded-full cursor-pointer hover:bg-slate-800 focus:outline-blue focus:bg-slate-800 focus:outline"
         >
           <IoSend fontSize={22} className="text-blue" />
         </button>
